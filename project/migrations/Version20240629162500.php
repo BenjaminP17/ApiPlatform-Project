ab5f9022a658963ccb7241c56ae145a2dbdbfb3f
Benjamin Pelissier <benjpls@gmail.com>
2024-06-29 18:28:03 +0200

    modification de la variable d'environnement

--- project/migrations/Version20240629162500.php
+++ project/migrations/Version20240629162500.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240629075950 extends AbstractMigration
+final class Version20240629162500 extends AbstractMigration
 {
     public function getDescription(): string
     {
